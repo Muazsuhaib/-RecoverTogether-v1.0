c565ab1eb5be1b90fb3d06f0b58dc9cf65454a71
Muazsuhaib <maazyuho@gmail.com>
2026-05-12 11:55:34 -0400

    Create MainTabView.swift

---
+++ RecoverTogether/App/MainTabView.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabView.swift
+//  RecoverTogether
+//
+//  Created by Muaz Fnu
+//
+//  DESCRIPTION:
+//  Main tab bar navigation.
+//  Tabs:
+//  - Home (DashboardView)
+//  - Calendar (CalendarView)
+//  - Chat (ChatView)
+//  - Friends (FriendsView)
+//  - Focus (FocusView)
+//  - Profile (ProfileView)
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    // MARK: - Properties
+    // Proprietary implementation
+
+    var body: some View {
+        // Tab bar with 6 tabs
+        // Proprietary implementation
+    }
+}
